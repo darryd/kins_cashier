e644d9fb922a3f923aba52eca93b94b8cc241229
darryd <darry.d@gmail.com>
2014-02-09 15:11:26 -0800

    trying

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,14 @@ class WelcomeController < ApplicationController
     
     question = params[:question]
     answer = params[:answer]
-    answer = answer.to_i
 
     actual_answer = Item.find_by(question: question).answer
-    actual_answer = actual_answer.to_i
 
     if actual_answer == answer
       flash['message'] = "Correct"
     else
       flash['message'] = "Incorrect. The correct code for " + question + " is " + actual_answer + "."
+      + 'You entered: ' + answer
     end
 
     redirect_to '/'
