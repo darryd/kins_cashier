eed354872461b3b70bd3a287094bd1bb39b3fe35
darryd <darry.d@gmail.com>
2014-02-09 14:11:48 -0800

    hope this works

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,21 @@
+class WelcomeController < ApplicationController
+  def index
+    @item = Item.all.sample(1).first
+  end
+
+  def test
+
+    question = params[:question]
+    answer = params[:answer].gsub(/\s+/, "")
+
+    actual_answer = Item.find_by(question: question)
+
+    if actual_answer == answer
+      flash['message'] = "Correct"
+    else
+      flash['message'] = "Incorrect. The correct code is " + actual_answer + "."
+    end
+
+    redirect_to '/'
+  end
+end
