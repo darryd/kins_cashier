491ee28bdbd26b49e0301da54241e063c23a1a15
darryd <darry.d@gmail.com>
2014-02-09 15:27:37 -0800

    sunshine and rain

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,16 @@ class WelcomeController < ApplicationController
 
     actual_answer = Item.find_by(question: question).answer
 
-    if actual_answer == answer
+    result = :success
+
+    begin
+      answer = answer.to_i
+      actual_answer.to_i
+    rescue
+      result = :failure
+    end
+
+    if actual_answer == answer and result == :success
       flash['message'] = "Correct"
     else
       flash['message'] = "Incorrect. The correct code for " + question + " is " + actual_answer + "." + "You entered: " + answer.to_s
