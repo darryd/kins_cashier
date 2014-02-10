9374a0045f029ed20aae3a2838ba622f42ddd763
darryd <darry.d@gmail.com>
2014-02-09 16:37:58 -0800

    working?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,7 +27,7 @@ class WelcomeController < ApplicationController
     actual_answer = item.answer
 
     if actual_answer.to_i == answer.to_i
-      flash['message'] = "Correct"
+      flash['message'] = "Correct!"
     else
       flash['message'] = "Try again! The correct code for " + question + " is " + actual_answer + "." + "You entered: " + answer.to_s
       flash[:id] = item.id
